fdc92f05b25af52cf7604d1dc519ce9aeddeba6d
xiaobingtech <developer@xiaobingkj.com>
2025-11-23 21:36:15 +0800

    初始提交

---
+++ m3u8Downloader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  m3u8Downloader
+//
+//  Created by fandong on 2025/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ m3u8Downloader/m3u8DownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  m3u8DownloaderApp.swift
+//  m3u8Downloader
+//
+//  Created by fandong on 2025/11/23.
+//
+
+import SwiftUI
+
+@main
+struct m3u8DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
